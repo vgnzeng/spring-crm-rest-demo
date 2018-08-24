305beb4f7055d41c4ff3e1159ab39db455e4598a
Guannian Zeng <vgnzeng@gmail.com>
2018-08-24 15:24:17 -0700

    added Controller method for quering one customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,16 @@ public class CustomerRestController {
 		
 		return customerService.getCustomers();
 		
+	}
+	
+	// add mapping for GET /customers/{customerId}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
 	}
 		
 	
