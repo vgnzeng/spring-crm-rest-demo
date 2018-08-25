2bc3ff6be99f9fa2cd1faa357634284e03b524c8
Guannian Zeng <vgnzeng@gmail.com>
2018-08-25 09:15:48 -0700

    added Exception class and @ControlAdvice for global expection handling
    @ContolAdvice is a specialization of @Component class, which can be used to enabling sharing across multiple @Contoller class

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -34,6 +34,10 @@ public class CustomerRestController {
 		
 		Customer theCustomer = customerService.getCustomer(customerId);
 		
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
 		return theCustomer;
 	}
 		
